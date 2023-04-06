a94725517e23c04a8921f2c71592d6d3aebbb7c0
MaRiAyakimovich <msm06@yandex.ru>
2023-04-06 11:20:47 +0300

    регистрация, авторизация, геометрия убраны

--- website_on_Python-main/main.py
+++ website_on_Python-main/main.py
@@ -33,12 +33,6 @@ def maths():
         return file.read()
 
 
-@app.route('/geometry')
-def geometry():
-    with open('templates/geometry.html', 'r', encoding='utf-8') as file:
-        return file.read()
-
-
 @app.route('/algebra')
 def algebra():
     with open('templates/algebra.html', 'r', encoding='utf-8') as file:
@@ -48,12 +42,6 @@ def algebra():
 @app.route('/algebra/log')
 def log():
     with open('templates/log.html', 'r', encoding='utf-8') as file:
-        return file.read()
-
-
-@app.route('/geometry/ygli')
-def ygli():
-    with open('templates/ygli.html', 'r', encoding='utf-8') as file:
         return file.read()
 
 
@@ -87,17 +75,6 @@ def physics():
         return file.read()
 
 
-app.route('/geometry/treugol')
-def treug():
-    return render_template('templates/treugol.html')
-
-
-@app.route('/geometry/priamie')
-def priamie():
-    with open('templates/priamie.html', 'r', encoding='utf-8') as file:
-        return file.read()
-
-
 @app.route('/physics/kinematika')
 def kinematika():
     with open('templates/kinematika.html', 'r', encoding='utf-8') as file:
@@ -114,46 +91,6 @@ def dinamika():
 def kolebania():
     with open('templates/kolebania.html', 'r', encoding='utf-8') as file:
         return file.read()
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.confirm.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.login.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            surname=form.surname.data,
-            email=form.login.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/subjects', methods=['GET', 'POST'])
